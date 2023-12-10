decf40176cf98ee440f1703d1cda8a19e3c7786e
satwik073 <satwikkanhere2003@gmail.com>
2023-12-10 20:11:58 +0530

    Updates

---
+++ precache-manifest.73d0634c70d10d69134ab05fb0c17c24.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74286688bdea183d008deff216d602af",
+    "url": "/SatwikPortFolio/index.html"
+  },
+  {
+    "revision": "a9560e82bbdffe5d40e0",
+    "url": "/SatwikPortFolio/static/css/2.3b91391f.chunk.css"
+  },
+  {
+    "revision": "a8de790fee08585a90b9",
+    "url": "/SatwikPortFolio/static/css/main.8ac94697.chunk.css"
+  },
+  {
+    "revision": "a9560e82bbdffe5d40e0",
+    "url": "/SatwikPortFolio/static/js/2.b281f408.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/SatwikPortFolio/static/js/2.b281f408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb5c8ba18b5d44c1a42",
+    "url": "/SatwikPortFolio/static/js/3.75e2788c.chunk.js"
+  },
+  {
+    "revision": "a8de790fee08585a90b9",
+    "url": "/SatwikPortFolio/static/js/main.2db79488.chunk.js"
+  },
+  {
+    "revision": "14bc836267dc70365673",
+    "url": "/SatwikPortFolio/static/js/runtime-main.a190fc64.js"
+  },
+  {
+    "revision": "5336bcc51a7714615cdddf998f65b93b",
+    "url": "/SatwikPortFolio/static/media/about.5336bcc5.png"
+  },
+  {
+    "revision": "b4e1f2ce7f883f01817e6032625f4c0a",
+    "url": "/SatwikPortFolio/static/media/cpp.b4e1f2ce.png"
+  },
+  {
+    "revision": "63fa4858b8cd8636adfbb51abaf3e9e0",
+    "url": "/SatwikPortFolio/static/media/css.63fa4858.png"
+  },
+  {
+    "revision": "1931427c23716bb4ff98d03627f7137c",
+    "url": "/SatwikPortFolio/static/media/fa-brands-400.1931427c.ttf"
+  },
+  {
+    "revision": "b6356c957274676e6571c1ff5e11c9a8",
+    "url": "/SatwikPortFolio/static/media/fa-brands-400.b6356c95.woff2"
+  },
+  {
+    "revision": "023a4a925fa3fce0f66b769ef6bbb264",
+    "url": "/SatwikPortFolio/static/media/fa-regular-400.023a4a92.woff2"
+  },
+  {
+    "revision": "a0cc1c8265e3163aa654a5284ea11ace",
+    "url": "/SatwikPortFolio/static/media/fa-regular-400.a0cc1c82.ttf"
+  },
+  {
+    "revision": "25914cfeafced317e1a0372187fbb2b9",
+    "url": "/SatwikPortFolio/static/media/fa-solid-900.25914cfe.ttf"
+  },
+  {
+    "revision": "6c4eee562650e53cee32496bdfbe534b",
+    "url": "/SatwikPortFolio/static/media/fa-solid-900.6c4eee56.woff2"
+  },
+  {
+    "revision": "71808e147dc6d82c198a4ba292c0cb69",
+    "url": "/SatwikPortFolio/static/media/fa-v4compatibility.71808e14.ttf"
+  },
+  {
+    "revision": "c9e50ccabe9e36f370272197595ea1e5",
+    "url": "/SatwikPortFolio/static/media/fa-v4compatibility.c9e50cca.woff2"
+  },
+  {
+    "revision": "5f2bfe656302d8a563dccf7ddbd2b154",
+    "url": "/SatwikPortFolio/static/media/firstMedal.5f2bfe65.png"
+  },
+  {
+    "revision": "7bda57b788b35cff0df1b1eceb16ade9",
+    "url": "/SatwikPortFolio/static/media/git.7bda57b7.png"
+  },
+  {
+    "revision": "78ca2c638ad6791ecee6360c191c9466",
+    "url": "/SatwikPortFolio/static/media/html.78ca2c63.png"
+  },
+  {
+    "revision": "b9052a08430cc647b8b1f80859f859b1",
+    "url": "/SatwikPortFolio/static/media/java.b9052a08.png"
+  },
+  {
+    "revision": "e5cd892423aa6cd626605ef15b2972bf",
+    "url": "/SatwikPortFolio/static/media/javascript.e5cd8924.png"
+  },
+  {
+    "revision": "d0baa16172adf71e8718034cd8e744b7",
+    "url": "/SatwikPortFolio/static/media/logo.d0baa161.png"
+  },
+  {
+    "revision": "719443269858865c5d2dd761bbcd3036",
+    "url": "/SatwikPortFolio/static/media/mongo.71944326.png"
+  },
+  {
+    "revision": "9aaf6c757fe2075784bb4bdf2b2fb2f0",
+    "url": "/SatwikPortFolio/static/media/nodejs.9aaf6c75.png"
+  },
+  {
+    "revision": "78dff019ce98da5d971eca4ec28851b5",
+    "url": "/SatwikPortFolio/static/media/project.78dff019.png"
+  },
+  {
+    "revision": "37201e97d9ee74bb294494a85ce21e5c",
+    "url": "/SatwikPortFolio/static/media/python.37201e97.png"
+  },
+  {
+    "revision": "117b22686ff6041845723faed419bc07",
+    "url": "/SatwikPortFolio/static/media/react.117b2268.png"
+  },
+  {
+    "revision": "93d3f105c4a8d3c37541f4de010b9f81",
+    "url": "/SatwikPortFolio/static/media/secondMedal.93d3f105.png"
+  },
+  {
+    "revision": "6e727de72fb8031bb890a0522cfacf6d",
+    "url": "/SatwikPortFolio/static/media/tailwind.6e727de7.png"
+  },
+  {
+    "revision": "5fc90d4a2e899d63e237ac18dc9cf533",
+    "url": "/SatwikPortFolio/static/media/vscode.5fc90d4a.png"
+  },
+  {
+    "revision": "c3989d77ae943a11072dad90d58117a9",
+    "url": "/SatwikPortFolio/static/media/wProject.c3989d77.png"
+  }
+]);
