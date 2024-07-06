3e80954b4640a016d71e9eb76cc750c776fa1ba4
satwik073 <satwikkanhere2003@gmail.com>
2024-07-06 18:35:37 +0530

    Updates

--- precache-manifest.edd3921df0a139cb6da9431386b5789f.js
+++ precache-manifest.edd3921df0a139cb6da9431386b5789f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "78b37c3906a6a98b598d030bbd020e3a",
+    "revision": "f1a37ec1a19b34bd8a4c706df5a40717",
     "url": "/SatwikPortFolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SatwikPortFolio/static/css/2.f115ba41.chunk.css"
   },
   {
-    "revision": "66bdc1cd40831bebae5b",
+    "revision": "9177d5f7892845564452",
     "url": "/SatwikPortFolio/static/css/main.7f765247.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SatwikPortFolio/static/js/3.995993cb.chunk.js"
   },
   {
-    "revision": "66bdc1cd40831bebae5b",
-    "url": "/SatwikPortFolio/static/js/main.efc33ac1.chunk.js"
+    "revision": "9177d5f7892845564452",
+    "url": "/SatwikPortFolio/static/js/main.89499543.chunk.js"
   },
   {
     "revision": "e3083aa4281add9c43c0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SatwikPortFolio/precache-manifest.c1b6fdd2cbc126e3c8c68ec0840d6ff3.js"
+  "/SatwikPortFolio/precache-manifest.edd3921df0a139cb6da9431386b5789f.js"
 );
 
 self.addEventListener('message', (event) => {
